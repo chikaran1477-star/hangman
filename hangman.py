64f884df9d76b7a71e1cd8e104102c0b668d87da
Chika <chikara.n.1477@gmail.com>
2026-03-01 05:37:08 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,50 @@
+import random
+
+wordlist= ["cat",
+           "cherry",
+           "tennis",
+           "orange",
+           "perfume"]
+
+wrong=0
+stages = ["",
+          "________       ",
+          "|              ",
+          "|       |      ",
+          "|       0      ",
+          "|     ／|＼    ",
+          "|     ／ ＼    ",
+          "|              ",
+          ""]
+word = random.choice(wordlist)
+rletters=list(word)
+board = ["_"]*len(word)
+win= False
+
+print("ハングマンへようこそ！")
+
+while wrong < len(stages)-1:
+    print("\n")
+    char = input("1文字を予想してね")
+
+    if char in rletters:
+        cind = rletters.index(char)
+        board[cind]=char
+        rletters[cind]='$'
+
+    else:
+        wrong += 1
+            
+    print(" ".join(board))
+    e=wrong + 1
+    print("\n".join(stages[0:e]))
+
+    if "_" not in board:
+        print("あなたの勝ち！")
+        print(" ".join(board))
+        win = True
+        break
+    
+if not win:
+    print("\n".join(stages[0:wrong+1]))
+    print("あなたの負け！正解は{}".format(word))
